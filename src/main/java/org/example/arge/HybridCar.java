acb8a500be81e39a99573cba7802dbd33cfaf250
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-07-09 17:32:31 +0300

    görev tamamlandı

---
+++ src/main/java/org/example/arge/HybridCar.java
@@ -0,0 +1,40 @@
+package org.example.arge;
+
+
+
+public class HybridCar extends CarSkeleton {
+    private double avgKmPerLiter;
+    private int batterySize;
+    private int cylinders;
+
+
+    public HybridCar(String name, String description, double avgKmPerLiter, int batterySize, int cylinders) {
+        super(name, description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    public double getAvgKmPerLiter() {
+        return avgKmPerLiter;
+    }
+
+    @Override
+    public String drive() {
+        return "run from Hybrid car";
+    }
+
+    @Override
+    public String startEngine() {
+        runEngine(this);
+        return getName() + "car starting...";
+    }
+}
